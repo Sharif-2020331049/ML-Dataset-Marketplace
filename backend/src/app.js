125e61696ff1fd84fe32d0f4ae30cce0d26158bd
Sahidur Rahman <sharif433742@gmail.com>
2025-06-11 16:30:26 +0600

    feat: Add Stripe dependencies for payment processing in frontend and backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,24 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser  from 'cookie-parser'
 import userRouter from './routes/user.route.js'
 import datasetRouter from './routes/dataset.route.js'
 import purchaseRouter from './routes/purchase.route.js'
+
 const app = express()
 
 // Middlewares 
-app.use([cors(), express.json(), express.urlencoded({extended: true})])
+app.use(cors({
+  origin: '*'
+}));
+
+app.use([express.json(), express.urlencoded({extended: true}), cookieParser()])
 
 // Api endpoints
 app.use('/api/v1/user', userRouter)
 app.use('/api/v1/dataset', datasetRouter)
-app.use('/api/v1/purchase', purchaseRouter)
+
+// payment
+app.use('/api/v1/dataset', purchaseRouter)
 
 export { app }
